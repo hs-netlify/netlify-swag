9ecdc03e47beddf3a400c9facf3e27fd0ec21781
Chris Hannaby <chris.hannaby@netlify.com>
2021-06-30 16:54:14 -0600

    hardcode translations

--- utils/useTranslations.js
+++ utils/useTranslations.js
@@ -1,8 +1,18 @@
-import translations from "../locales/active.json";
+const translations = {
+  es: {
+    home_title: "Casa",
+    about_title: "Acerca de",
+  },
+  en: {
+    home_title: "Home",
+    about_title: "About",
+  },
+};
 
 export default function useTranslations() {
+  const locale = process.env.NEXT_PUBLIC_LOCALE;
   function t(key) {
-    return translations[key];
+    return translations[locale][key];
   }
   return t;
 }
