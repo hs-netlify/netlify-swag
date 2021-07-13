08fdcb3860f8ca44265787b64f1b75aa961adca4
Chris Hannaby <chris.hannaby@netlify.com>
2021-07-12 19:19:53 -0600

    handle build hook from prismic

---
+++ www/functions/handleBuildHook.js
@@ -0,0 +1,27 @@
+const Prismic = require("@prismicio/client");
+const fetch = require("node-fetch");
+
+var apiEndpoint = "https://netlifyswag.cdn.prismic.io/api/v2";
+
+const buildHooks = {
+  en: "https://api.netlify.com/build_hooks/60ece721ccc8c3501d62be62",
+  es: "https://api.netlify.com/build_hooks/60ece7d77f8b5c5235bd67f3",
+};
+
+exports.handler = async (event) => {
+  const documentId = JSON.parse(event.body).documents[0];
+  if (documentId) {
+    const api = await Prismic.client(apiEndpoint);
+    const document = await api.getByID(documentId);
+    const lang = document.lang;
+    const buildHook = buildHooks[lang.split("-")[0]];
+    if (buildHook) {
+      await fetch(buildHook, {
+        method: "POST",
+      });
+    }
+  }
+  return {
+    statusCode: 200,
+  };
+};
