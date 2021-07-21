a4f9ec3148569501bb71d5535c73dac9a136ac4c
Chris Hannaby <chris.hannaby@netlify.com>
2021-07-20 18:22:00 -0600

    don't use basePath

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   target: "serverless",
-  basePath: `/${process.env.NEXT_PUBLIC_LOCALE}`,
+  //basePath: `/${process.env.NEXT_PUBLIC_LOCALE}`,
   images: {
     domains: ["cdn.shopify.com"],
   },
