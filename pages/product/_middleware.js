4913f45a15ea9116f4e33b6a398073a0c6de5c8b
Chris Hannaby <chris.hannaby@netlify.com>
2022-06-28 18:49:23 -0600

    add basic auth

--- pages/product/_middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(req) {
-  const url = req.nextUrl.clone();
-  url.pathname = `/product/classic-netlify-sweatshirt`;
-  const res = NextResponse.rewrite(url);
-  return res;
-}
