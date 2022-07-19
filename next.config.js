3c4919fa31cdddd079b6aae0b56a1ff682511b79
Chris Hannaby <chris.hannaby@netlify.com>
2022-07-19 14:56:47 -0600

    redirect to strip .html

--- next.config.js
+++ next.config.js
@@ -2,4 +2,13 @@ module.exports = {
   images: {
     domains: ["cdn.shopify.com", "tailwindui.com", "files.stripe.com"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/:path*(.html)",
+        destination: "/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
