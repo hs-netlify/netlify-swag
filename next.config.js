6f8a89e73c4d62e6603b55c11114174f8ea223d9
Chris Hannaby <chris.hannaby@netlify.com>
2022-08-30 15:55:51 -0600

    remove redirects

--- next.config.js
+++ next.config.js
@@ -2,13 +2,4 @@ module.exports = {
   images: {
     domains: ["cdn.shopify.com", "tailwindui.com", "files.stripe.com"],
   },
-  async redirects() {
-    return [
-      {
-        source: "/:path*(.html)",
-        destination: "/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
