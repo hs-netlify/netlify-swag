eee235c2c96156fd1a738466db6570d394de3003
Chris Hannaby <chris.hannaby@netlify.com>
2022-09-01 16:45:12 -0600

    change padding

--- components/FeaturedProducts.js
+++ components/FeaturedProducts.js
@@ -3,7 +3,7 @@ import ProductCard from "./ProductCard";
 export default function FeaturedProducts({ title, products }) {
   return (
     <div className="bg-white">
-      <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="max-w-2xl mx-auto py-8 px-4 sm:py-12 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="md:flex md:items-center md:justify-between">
           <h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
             {title}
