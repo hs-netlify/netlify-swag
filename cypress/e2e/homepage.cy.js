32cbe3f457e8ff4f97bb58bfc279b2a235b6af27
Henry Smith <henry.smith@netlify.com>
2022-10-28 10:12:12 +0100

    Update

--- cypress/e2e/homepage.cy.js
+++
@@ -1,7 +0,0 @@
-describe("Homepage", () => {
-    it("Should remove cookie banner", () => {
-        cy.visit("/");
-        cy.get('.order-3 > [data-test="submit-button"]').click();
-        cy.get('.order-3 > [data-test="submit-button"]').should('not.exist');
-    })
-})
